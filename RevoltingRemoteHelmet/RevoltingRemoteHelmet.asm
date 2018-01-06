347b982de7c0f51e47f7cdcc7831ee54a3ffaed8
Philipp Fuerholz <fuerholz@gmx.ch>
2018-01-06 14:35:26 +0100

    initial commit

---
+++ RevoltingRemoteHelmet/RevoltingRemoteHelmet.asm
@@ -0,0 +1,227 @@
+/*
+ * RevoltingRemoteHelmet.asm
+ *
+ *  Created: 25.11.2017 13:23:09
+ *   Author: fuerh_000
+ */ 
+
+
+ #include <m8def.inc>
+ #include "GeneralSettings.inc"
+
+ .equ channel_nr = 0x12
+ .equ secretnumber_1 = 0x42
+ .equ secretnumber_2 = 0x32
+ .equ min_pulses = 0x08
+ .org 0x000
+rjmp reset
+
+
+.org ICP1addr
+rjmp inputCapture
+
+.org OVF0addr
+rjmp timer0_ovflow
+
+.org OVF1addr
+rjmp timer_stop
+
+
+// hauptprogramm startet hier
+.org 0x013
+reset:
+ldi r16, high(RAMEND)
+out SPH,r16
+ldi r16, low(RAMEND)
+out SPL,r16
+
+
+// ADC0-2 als Eingänge
+ldi r16,(0<<DDC0)|(0<<DDC1)|(0<<DDC2)
+out DDRC,r16
+
+// adc einrichten
+// AVCC mit kapazität am Aref-Pin, Resultat linksbündig
+ ldi r16,(1<<REFS0)|(1<<ADLAR)
+ out ADMUX,r16
+
+ // prescaler 8, enabled, do not start conversion
+ ldi r16,(1<<ADEN)|(0<<ADSC)|(1<<ADPS1)|(1<<ADPS0)
+ out ADCSRA,r16
+
+
+
+
+
+// variablen mit standardwerten initialisieren
+ldi r16,0x00
+sts adc_counter,r16
+sts receiver_state,r16
+sts hsv_angle,r16
+sts hsv_angle+1,r16
+sts decoder_cntr,r16
+
+
+
+// PB1-PB3 as output
+ldi r16,(1<<DDB1)|(1<<DDB2)|(1<<DDB3)|(0<<DDB0)
+out DDRB,r16
+ldi r16,0x00
+out PORTB,r16
+// PD0 aus input
+ldi r16,(0<<DDD0)
+out DDRD,r17
+
+// timer/counter 1 acts as clock cycle counter for icp pin
+// overflow on OCR1A
+ldi r16,(1<<WGM10)|(1<<WGM11)
+out TCCR1A,r16
+ldi r16,(1<<TICIE1)|(1<<OCIE1A)
+out TIMSK,r16
+ldi r17,high(pulse_duration+pulse_tolerance)
+ldi r16,low(pulse_duration+pulse_tolerance)
+out OCR1AH,r17
+out OCR1AL,r16
+ldi r16,(1<<ICNC1)|(1<<ICES1)|(1<<CS10)|(1<<WGM13)|(1<<WGM12) // enable noise cancelling, enable trigger on rising edge, start clock
+out TCCR1B,r16
+
+
+
+sei // enable interrupts
+main:
+
+lds r16,decoder_cntr
+cpi r16,min_pulses
+brne led_off
+in r16,PORTB
+ori r16,0x02
+out PORTB,r16
+
+rjmp main
+led_off:
+in r16,PORTB
+andi r16,0xff-0x02
+out PORTB,r16
+
+rjmp main
+
+//rcall longwait
+
+
+ 
+
+
+
+// waits for about a quarter of a second
+longwait:
+push r16
+push r17
+push r18
+push r19
+ldi r18,0x00
+
+start_clock:
+ldi r16,0x00
+out TCNT0,r16
+ldi r16,(0<<CS02)|(1<<CS01)|(0<<CS00)
+out TCCR0,r16
+wait_oflow:
+in r17,TIFR
+sbrs r17,TOV0
+rjmp wait_oflow
+out TIFR,r17
+//inc r18
+//brne start_clock
+pop r19
+pop r18
+pop r17
+pop r16
+ret
+
+
+
+inputCapture:
+push r16
+in r16,SREG
+push r16
+
+in r16,ICR1L
+in r17,ICR1H
+subi r16,low(pulse_duration+pulse_tolerance)
+sbci r17,high(pulse_duration+pulse_tolerance)
+brmi inputCapture_cont1
+rjmp inputCapture_error
+inputCapture_cont1:
+in r16,ICR1L
+in r17,ICR1H
+subi r16,low(pulse_duration-pulse_tolerance)
+sbci r17,high(pulse_duration-pulse_tolerance)
+brpl inputCapture_cont2
+rjmp inputCapture_error
+inputCapture_cont2:
+// pulse had the correct duration
+
+// invert edge trigger slope
+in r16,TCCR1B
+ldi r17,(1<<ICES1)
+eor r16,r17
+out TCCR1B,r16
+
+// reset counter
+ldi r16,0x00
+out TCNT1H,r16
+out TCNT1L,r16
+
+// increase the decoder counter
+lds r16,decoder_cntr
+cpi r16,min_pulses
+breq inputCapture_end
+inc r16
+sts decoder_cntr,r16
+rjmp inputCapture_end
+
+inputCapture_error:
+ldi r16,0x00 // reset the decoder counter to zero
+sts decoder_cntr,r16
+
+
+
+inputCapture_end:
+pop r16
+out SREG,r16
+pop r16
+reti
+
+timer0_ovflow:
+reti
+
+
+timer_stop:
+push r16
+in r16,SREG
+push r16
+
+ldi r16,0x00 // reset the decoder counter to zero
+sts decoder_cntr,r16
+
+pop r16
+out SREG,r16
+pop r16
+reti
+
+.dseg
+.org SRAM_START
+adc_counter:
+.byte 1
+receiver_state:
+.byte 1
+red_val:
+.byte 1
+green_val:
+.byte 1
+blue_val:
+.byte 1
+hsv_angle: // erstes byte: teilwinkel von 0 bis 255, zweites bytes: phase von 0 bis 5
+.byte 2
+decoder_cntr:
+.byte 1
---
+++ RevoltingRemoteHelmet/helmet_sender.asm
@@ -0,0 +1,357 @@
+/*
+ * helmet_sender.asm
+ *
+ *  Created: 19.12.2017 20:58:04
+ *   Author: fuerh_000
+ */ 
+
+  #include <m8def.inc>
+  #include "GeneralSettings.inc"
+
+  /*
+    code für den sender, ein Schalter ist an PC0 angehängt, dieser schaltet den Helm ein- und aus
+    das kontrollicht and pc1
+
+	der Sender schickt ein Manchester-encodiertes Signal welchem 4 1-0 durchgänge vorangestellt sind, danach kommt ein byte Information, danach wird nochmals ein 
+	 "1" von mindestens 4 durchgängen länge gesendet
+  */
+
+ .equ channel_nr = 0x12
+ .equ secretnumber_1 = 0x42
+ .equ secretnumber_2 = 0x32
+ .org 0x000
+rjmp reset
+
+
+.org OC1Aaddr
+rjmp oscillator_handler
+
+.org OVF0addr
+rjmp debounce_stop
+
+.org 0x013
+reset:
+ldi r16, high(RAMEND)
+out SPH,r16
+ldi r16, low(RAMEND)
+out SPL,r16
+
+
+
+
+//port c konfigurieren
+// PC0: LED (output)
+// PC1: schalter (input)
+// PC2: digitaler ausgang
+ldi r16, (1<<DDC0)|(0<<DDC1)|(1<<DDC2)
+out DDRC,r16
+ldi r16,(1<<PORTC1)
+out PORTC,r16
+
+//portb konfigurieren: portb1 ist ausgang
+ldi r16,(1<<DDB1)
+out DDRB,r16
+
+// setup counter 1 as CTC (clear timer on compare)
+// enable interrupt on output compare 1 A and Overflow on Counter 0
+ldi r16,(1<<OCIE1A)|(1<<TOIE0)
+OUT TIMSK,r16
+ldi r16,(1<<WGM12) // CTC mode
+out TCCR1B,r16
+
+ldi r16,0x02
+sts button_state,r16
+
+sei
+
+
+main:
+
+in r16,PINC
+andi r16,0x02
+cpi r16,0x02
+brne check_off
+lds r17,button_state
+cp r16,r17
+breq main
+sts button_state,r16
+in r17,TCCR0
+cpi r17,0x00
+brne main
+
+// start debounce_wait
+ldi r16,(1<<CS02)
+out TCCR0,r16
+
+rjmp main
+
+check_off:
+
+lds r17,button_state
+cp r16,r17
+breq main
+sts button_state,r16
+in r17,TCCR0
+cpi r17,0x00
+brne main
+
+lds r16,sender_state
+cpi r16,0x00 
+brne main // the sender is already sending, sender_state is greater than zero
+
+// start debounce_wait
+ldi r16,(1<<CS02)
+out TCCR0,r16
+
+ldi r17,0b01010101
+ldi r16,0b00000100 // switch on red on channel 0, the uppermost 5 bits are the channel number and the lower three bytes are the RGB bits
+lsl r16
+rol r17
+sts message_word+1,r17
+sts message_word,r16
+ldi r16,0x00
+inc r16
+sts sender_state,r16
+
+ldi r17,high(pulse_duration)
+ldi r16,low(pulse_duration)
+out OCR1AH,r17
+out OCR1AL,r16
+in r16,TCCR1A
+andi r17,(1<<COM1A0)
+out TCCR1A,r16
+in r16,TCCR1B
+ori r16,(1<<CS10) // start clock, output should be zero, wait for 1 pulse_duration
+out TCCR1B,r16
+
+in r16,PORTC
+ori r16,0x04
+out PORTC,r16
+
+rjmp main
+
+
+
+
+
+
+// waits for about a quarter of a second
+longwait:
+push r16
+push r17
+push r18
+push r19
+ldi r18,0x00
+
+start_clock:
+ldi r16,0x00
+out TCNT0,r16
+ldi r16,(1<<CS02)|(0<<CS01)|(1<<CS00)
+out TCCR0,r16
+wait_oflow:
+in r17,TIFR
+sbrs r17,TOV0
+rjmp wait_oflow
+out TIFR,r17
+inc r18
+cpi r18,0x0C
+brne start_clock
+pop r19
+pop r18
+pop r17
+pop r16
+ret
+
+
+
+oscillator_handler:
+push r16
+push r17
+push r18
+push r19
+in r16,SREG
+push r16
+
+
+/* simulate output of OCR1 by toggling PC0*/
+in r16,PORTC
+ldi r17,0x01
+eor r16,r17
+out PORTC,r16
+
+/*
+first call of the interrupt handler after transmission has started
+*/
+lds r16,sender_state
+andi r16,0b00111111
+cpi r16,0x01
+brne oh_check_sender_state
+// "bits sent" counter is one
+inc r16
+lds r18,message_word+1
+lds r17,message_word
+lsl r17
+rol r18
+brcc clearlastbit// last bit is a "0"
+sbr r16,128 // set the last bit, last bit sent was a "1" 
+rjmp oh_cont1
+clearlastbit:
+cbr r16,128
+oh_cont1:
+sts message_word+1,r18
+sts message_word,r17
+sts sender_state,r16
+ldi r17,high(2*pulse_duration)
+ldi r16,low(2*pulse_duration)
+out OCR1AH,r17
+out OCR1AL,r16
+rjmp oh_end
+
+/*message transmitted, transmit the end part consisting of 5 times pulse_duration of "1" or 1 pulse duration of zero followed by 4 pulse durations of 1*/
+oh_check_sender_state:
+cpi r16,0x10 // 16 bits transmitted, send a "1" that lasts 4 pulse_duration's
+brne oh_check_sender_state2
+lds r16,sender_state
+sbrc r16,7
+rjmp end_with_one // send one clock cycle with 0 level and 4 clock cycles with 1 level
+sbrc r16,6
+rjmp onepulse2 // finish second half "same-bit" pulse
+inc r16
+sts sender_state,r16
+
+ldi r17,high(5*pulse_duration)
+ldi r16,low(5*pulse_duration)
+out OCR1AH,r17
+out OCR1AL,r16
+rjmp oh_end
+
+end_with_one:
+inc r16
+sts sender_state,r16
+
+ldi r17,high(pulse_duration)
+ldi r16,low(pulse_duration)
+out OCR1AH,r17
+out OCR1AL,r16
+rjmp oh_end
+
+
+/*
+at the very end: switch everything off when 5 pulse durations of one were transmitted or send out another pulse of 4 pulse durations
+*/
+oh_check_sender_state2:
+cpi r16,0x11 // seventeenth bit set, long pulse ends
+brne oh_check_sender_state3
+lds r16,sender_state
+sbrc r16,7
+rjmp end_with_one2
+
+// switch off clock
+andi r17,0xFF-0x07
+out TCCR1B,r17
+ldi r16,0x00
+sts sender_state,r16
+
+// reset counter
+out TCNT1H,r16
+out TCNT1L,r16
+rjmp oh_end
+
+end_with_one2:
+cbr r16,128 // clear the bit declaring 1 was the last bit sent making it jump into the "very end" part on next interrupt
+sts sender_state,r16
+ldi r17,high(4*pulse_duration)
+ldi r16,low(4*pulse_duration)
+out OCR1AH,r17
+out OCR1AL,r16
+
+
+rjmp oh_end
+
+/* 
+in the middle of transmission, first bit already transmitted
+*/
+oh_check_sender_state3:
+lds r16,sender_state
+sbrc r16,6 // bit 6 indicates that only one pulse of length pulse_duration has been sent, automatically send the second one
+rjmp onepulse2
+inc r16
+mov r19,r16 // copy r16 to r19
+lds r18,message_word+1
+lds r17,message_word
+lsl r17
+rol r18
+brcc clearlastbit2// last bit is a "0"
+sbr r16,128 // set the last bit, last bit sent was a "1" 
+rjmp oh_cont2
+clearlastbit2:
+cbr r16,128
+oh_cont2:
+sts message_word+1,r18
+sts message_word,r17
+sts sender_state,r16
+// check if last bit sent and current bit is different
+andi r16,0x80
+andi r19,0x80
+eor r16,r19
+cpi r16,0x00
+breq onepulse1 // bits are equal, send a pulse of only half duration
+
+// send a double pulse
+ldi r17,high(2*pulse_duration)
+ldi r16,low(2*pulse_duration)
+out OCR1AH,r17
+out OCR1AL,r16
+rjmp oh_end
+
+
+
+onepulse1:
+lds r16,sender_state
+sbr r16,64
+sts sender_state,r16
+ldi r17,high(pulse_duration)
+ldi r16,low(pulse_duration)
+out OCR1AH,r17
+out OCR1AL,r16
+rjmp oh_end
+
+onepulse2:
+cbr r16,64
+sts sender_state,r16
+ldi r17,high(pulse_duration)
+ldi r16,low(pulse_duration)
+out OCR1AH,r17
+out OCR1AL,r16
+
+oh_end:
+pop r16
+out SREG,r16
+pop r19
+pop r18
+pop r17
+pop r16
+reti
+
+debounce_stop:
+push r16 
+in r16,SREG
+push r16
+
+ldi r16,0x00
+out TCCR0,r16
+out TCNT0,r16
+
+pop r16
+out SREG,r16
+pop r16
+reti
+
+.dseg
+.org SRAM_START
+sender_state:
+.byte 1
+button_state:
+.byte 1
+message_word:
+.byte 2
